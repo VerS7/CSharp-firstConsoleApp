f6135c7dbaa8f324a5e51328535016f821a2ccd2
Davydov Albert <103847309+VerS7@users.noreply.github.com>
2022-10-08 00:26:21 +0300

    .

---
+++ firstConsoleApp/AvgGeometric.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+
+namespace firstConsoleApp
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Введите первое число:");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите второе число:");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            //модуль числа
+            if (a < 0)
+                a = -a;
+            if (b < 0)
+                b = -b;
+            //Среднее геометрическое
+            Console.WriteLine("Среднее геометрическое модулей числа a и b: {0:F}", Math.Sqrt(a * b));
+        }
+    }
+}
